8c0297b692d68e606f555e1bcd95364246b056b3
sonam_kumari <kumarisonamam001998@gmail.com>
2026-03-15 18:30:32 +0530

    powerBI connection and SQL integration

--- investigate_bt_data.py
+++ investigate_bt_data.py
@@ -38,3 +38,40 @@ bruteforce_suspects =failed_logins [failed_logins > 3]
 
 night_logins = df[(df['Action'] == 'LOGIN') & (df['Timestamp'].dt.hour < 5)]
 
+
+
+
+#---REPORT GENERATION---
+print(f" FOUND{len(unauthorized_changes)} UNAUTHORIZED CONFIG CHANGES:")
+
+if not unauthorized_changes.empty: 
+    print(unauthorized_changes[['Timestamp', 'User_ID', 'System_Name']].head())
+
+    print(f"\n BRUTEFORCE SUSPECTS (Multiple Failed Logins):")
+    print(bruteforce_suspects if not bruteforce_suspects.empty else "None")
+    print(f"\n SUSPICIOUS NIGHT LOGINS DETECTED: {len(night_logins)}")
+
+    print(f"\n NIGHT LOGINS (12 AM - 5 AM):")
+    print(night_logins[['Timestamp', 'User_ID', 'IP_Address']].head() if not night_logins.empty else "None")
+else:
+    print("No Unauthorized Changes Found")
+
+
+#4. Export for Stakeholders (Power BI / Excel)
+# We will create a cleaned 'Alert Report' that we can import into Power BI.
+unauthorized_changes.to_csv('bt_alerts_report.csv', index=False)
+print("\n Investigation Report saved as 'bt_alerts_report.csv' for Power BI.")
+
+
+#1. SQL Engine banayein
+
+# Password ki jagah apna MySQL password likhein (Example: 'root123')
+
+engine = create_engine('mysql+mysqlconnector://root:Sonam1998@localhost/bt_sentinel_db')
+
+
+#2. DataFrame(df) ko direct SQL Table mein upload karein
+#"replace" ka matlab hai har bar naya data purane data ko update kar dega.
+df.to_sql('bt_system_logs', con=engine, if_exists='replace', index=False)
+print("\n Success: Data Python se MySQL Database mein chala gaya hai!")
+
